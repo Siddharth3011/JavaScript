7f3183ba736191be3762f2f551db4973a05a9dbc
Siddharth Pandey <siddharthpandey2911@gmail.com>
2026-09-01 01:08:43 +0530

    Refactor IIFE comments for clarity and detail
    Updated comments and explanations for IIFE usage.

--- 03_basics/04_IIFE.js
+++ 03_basics/04_IIFE.js
@@ -7,11 +7,12 @@
 // chai()
 
 //Using IIFE:
-
+//aab agr database ko immediatly invoke krna hai to last mein paranthesis:() lga denge. but fir wo error dene lagega to fir ham function ko bhi ek paranthesis mein rakh dete hai, tkai wo bhi ek block bn jaye
 //Named IIFE:
 (function chai(){
     console.log("DB Connected")
-})(); //yahan apko semicolon explicitely lgana padta hai taki 2 IIFE ek sath likh sake.
+})(); //yahan apko semicolon explicitely lgana padta hai kyuki iife ko pta heen nhi hai ki use context rokna kahan hai aur tb ham 2 IIFE ek sath likh sakte hai.
+
 //Unnamed IIFE:
 ((name) => {
     console.log(`Db Connected two ${name}`);
