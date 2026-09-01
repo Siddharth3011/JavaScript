3333f62c4c507ff65ddfdefd72d35589b0d97024
Siddharth Pandey <siddharthpandey2911@gmail.com>
2026-09-02 03:05:33 +0530

    Enhance comments on method chaining in chainig_of_map.js
    Added comments explaining method chaining with map and filter.

--- 05_iterations/chainig_of_map.js
+++ 05_iterations/chainig_of_map.js
@@ -3,7 +3,9 @@ const myNumbers = [1,2,3,4,5,6,7,8,9,10]
 // console.log(newNums);
 
 const newNums = myNumbers
-                        .map((num)=>num*10)
+                        .map((num)=>num*10) //yeh hai chaining of methods kehte hai isme pehla wala method dusre wale ko value return krta hai aur tb dusra wala usme chnaged krta hai.
                         .map((num)=>num+1)
                         .filter((num)=>num>=40)
+
+//ek baat dhyan rakhna ki map new modifies value ko return krta hai jbki filter boolean return krta hai, map data ko transform krta hai jbki filter select krta hai.
 console.log(newNums);
